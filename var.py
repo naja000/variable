5c8895f44d6a04603cfcdab2b9504eb7228047d9
naja000 <110969361+naja000@users.noreply.github.com>
2022-08-24 14:26:12 +0530

    Add files via upload

---
+++ var.py
@@ -0,0 +1,26 @@
+var="Thanha"
+print(var.isidentifier())
+var="Thanha12"
+print(var.isidentifier())
+var="_thanha"
+print(var.isidentifier())
+var="12thanha"
+print(var.isidentifier())
+var="thanha%"
+print(var.isidentifier())
+var="&thanha"
+print(var.isidentifier())
+var="thn thn"
+print(var.isidentifier())
+var="thanha thanha"
+print(var.isspace())
+var="     "
+print(var.isspace())
+var_title="I am The Best"
+print(var_title.istitle())
+var_title="I Am The Best"
+print(var_title.istitle())
+is_upper="I AM THE BEST"
+print(is_upper.isupper())
+is_lower="i am the best"
+print(is_lower.islower())
